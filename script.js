5e1a0fcb997ce1bba5cd94050c4683bfe73fb17c
hipopridwan <ridwanhipop@gmail.com>
2022-07-06 19:00:12 +0600

    responsive navbar added

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+
+// unused
+// if (bar) {
+//   bar.addEventListener("click", () => {
+//     nav.classList.add("active");
+//   });
+// }
+// if (close){
+//     close.addEventListener('click',()=>{
+//         nav.classList.remove('active');
+//     })
+// }
+
+// this is for navbar open and outside click close...
+document.onclick = function (e) {
+  if (e.target.id !== "bar" && e.target.id !== "navbar") {
+    bar.classList.remove("active");
+    nav.classList.remove("active");
+  }
+};
+bar.onclick = function () {
+  bar.classList.toggle("active");
+  nav.classList.toggle("active");
+};
